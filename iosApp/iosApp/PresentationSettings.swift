f41c0a0f70a94bbca756a86bb8098692aa9e1d2c
Ricardo Bravo <ricardobravo@bcp.com.pe>
2022-03-31 00:25:38 -0500

    feat(iOS): add swift ui views

---
+++ iosApp/iosApp/PresentationSettings.swift
@@ -0,0 +1,16 @@
+//
+//  PresentationSettings.swift
+//  iosApp
+//
+//  Created by Ricardo Bravo on 31/03/22.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+class TimezoneVariables: ObservableObject {
+  @Published var showTimezoneDialog = false
+}
+class FindMeetingVariables: ObservableObject {
+  @Published var showHoursDialog = false
+}
